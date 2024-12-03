202efd689eccb09a48c03d2bf68dec9906da6b6b
julitru <137878941+julitru@users.noreply.github.com>
2024-12-03 15:25:12 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  // Get the member's skills
+  let skills = this.skills;
+  // Get the member's name
+  let name = this.name;
+  // Return the member's name and skills
+  console.log(`${name}'s skills: ${skills}`);
+}
